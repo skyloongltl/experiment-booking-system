be8cc8695e2c7b41af85f4cca9665f23f1ba40d0
skyloongltl <skyloongltl@gmail.com>
2018-03-30 17:34:45 +0800

    add rate limit

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,19 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1',
+$api->version(
+    'v1',
     [
         'namespace' => 'App\Http\Controllers\Api'
     ], function ($api) {
-        $api->post('test', 'TestController@test')
-            ->name('api.test.test');
-});
+        $api->group(
+            [
+                'middleware' => 'api.throttle',
+                'limit' => config('api.rate_limits.access.limit'),
+                'expires' => config('api.rate_limits.access.expires'),
+            ], function ($api) {
+            $api->post('test', 'TestController@test')
+                ->name('api.test.test');
+        });
+    }
+);
